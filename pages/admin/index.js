6580412dd8a5c68fcf5dd58327d9a5c1873f754d
Sinas66 <Pashkomania@gmail.com>
2019-09-16 19:02:07 +0300

    ssr

---
+++ pages/admin/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class adminPage extends Component {
+  render() {
+    return <div>admin2</div>;
+  }
+}
+
+export default adminPage;
---
+++ pages/restore/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Restore from "../../components/Restore/Restore";
+
+const restorePage = params => (
+  <>
+    <Restore params={params} />
+  </>
+);
+
+export default restorePage;
